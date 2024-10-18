d5054e6b5e67e23b095bdd8c21fc86973757edab
Annie McMahon <108587699+AnnieMcMahon@users.noreply.github.com>
2024-10-18 13:09:56 -0400

    developed login page logic

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,30 +1,45 @@
 "use client";
 import "./login.css";
-// import appUsers from '../assets/appUsers';
+import appUsers from '../assets/appUsers';
 
 function Login() {
   function handleSubmit(e) {
     e.preventDefault();
-    const userEmail = e.target.value;
+    const userEmail = e.target.userEmail.value;
+    const userPassword = e.target.userPassword.value;
+    let userStatus = "";
+    if (userEmail.length > 0 && userPassword.length > 0) {
+      const userInfo = appUsers.find(user => user.email == userEmail);
+      if (userInfo) {
+        if (userInfo.password == userPassword) {
+          userStatus = "validated";
+        } else {
+          userStatus ="password";
+        }
+      } else {
+        userStatus = "new";
+      }
+    } else {
+      console.log("e-mail and/or password missing");
+    }
+    console.log(userStatus);
   }
 
   return (
     <div id="login">
       <h1>Log In</h1>
       <div id="login-info" className="text-box">
-        <h2>Fill out the information</h2>
         <form onSubmit={(e) => handleSubmit(e)}>
           <label htmlFor="userEmail">E-mail: </label>
           <input type="email" name="email" id="userEmail" autoComplete="true" />
-          <button type="submit" className="enter-button">
-            Enter
+          <br />
+          <label htmlFor="userPassword">Password: </label>
+          <input type="password" name="password" id="userPassword" autoComplete="false" />
+          <br />
+          <button type="submit" className="form-button">
+            Log In
           </button>
         </form>
-        <br />
-
-        <br />
-        <br />
-        {/* <button type="submit" className="form-button">Submit Form</button> */}
       </div>
     </div>
   );
