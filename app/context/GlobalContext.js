a219735e5bc19710875690fd4394409b9e9f1f20
Annie McMahon <108587699+AnnieMcMahon@users.noreply.github.com>
2024-10-21 17:17:54 -0400

    simplified GlobalContext for testing purposes

--- app/context/GlobalContext.js
+++ app/context/GlobalContext.js
@@ -6,74 +6,49 @@ import sampleAppUsers from "../assets/sampleAppUsers";
 const GlobalContext = createContext();
 
 export function GlobalProvider({ children }) {
-  //Initialize states with sample data for testing purposes
+  //Initialize states with sample data
   const [appUsers, setAppUsers] = useState(sampleAppUsers);
   const [currentUser, setCurrentUser] = useState(sampleAppUsers[0]);
   const [hikes, setHikes] = useState(sampleHikes);
 
-  //Actual Code: 
-
   useEffect(() => {
-    
     const storedUsers = JSON.parse(localStorage.getItem("appUsers"));
     const storedHikes = JSON.parse(localStorage.getItem("hikes"));
     const storedUser = JSON.parse(localStorage.getItem("currentUser"));
-
-    if (storedUsers) {
-      setAppUsers(storedUsers);
-    } else {
-      localStorage.setItem("appUsers", JSON.stringify(sampleAppUsers));
-    }
-
-    if (storedHikes) {
-      setHikes(storedHikes);
-    } else {
-      localStorage.setItem("hikes", JSON.stringify(sampleHikes));
-    }
-
-    if (storedUser) {
-      setCurrentUser(storedUser);
-    } else {
+    console.log("stored values at the beginning of useEffect: ", storedUsers, storedHikes, storedUser);
+    if (!storedUser) {
       localStorage.setItem("currentUser", JSON.stringify(sampleAppUsers[0]));
     }
-
-        //Verify data is correct on state and local storage
-        console.log(hikes);
-        console.log(appUsers);
-        console.log(currentUser);
-        console.log(JSON.parse(localStorage.getItem("hikes")));
-        console.log(JSON.parse(localStorage.getItem("appUsers")));
-        console.log(JSON.parse(localStorage.getItem("currentUser")));
   }, []);
   
+  // Store info in localStorage when it changes
+  // useEffect(() => {
+  //   localStorage.setItem("hikes", JSON.stringify(hikes));
+  // }, [hikes]);
 
-  // Store current user in localStorage when it changes
-  useEffect(() => {
-    localStorage.setItem("hikes", JSON.stringify(hikes));
-  }, [hikes]);
-  useEffect(() => {
-    localStorage.setItem("appUsers", JSON.stringify(appUsers));
-  }, [appUsers]);
-  useEffect(() => {
-    localStorage.setItem("currentUser", JSON.stringify(currentUser));
-  }, [currentUser]);
+  // useEffect(() => {
+  //   localStorage.setItem("appUsers", JSON.stringify(appUsers));
+  // }, [appUsers]);
+  
+  // useEffect(() => {
+  //   localStorage.setItem("currentUser", JSON.stringify(currentUser));
+  // }, [currentUser]);
 
   //Load initial data in localstorage and state for testing purposes
-  /*useEffect(() => {
-    localStorage.clear();
-    localStorage.setItem("hikes", JSON.stringify(sampleHikes));
-    localStorage.setItem("appUsers", JSON.stringify(sampleAppUsers));
-    localStorage.setItem("currentUser", JSON.stringify(sampleAppUsers[0]));
+  // useEffect(() => {
+  //   localStorage.clear();
+  //   localStorage.setItem("hikes", JSON.stringify(sampleHikes));
+  //   localStorage.setItem("appUsers", JSON.stringify(sampleAppUsers));
+  //   localStorage.setItem("currentUser", JSON.stringify(sampleAppUsers[0]));
 
-    //Verify data is correct on state and local storage
-    console.log(hikes);
-    console.log(appUsers);
-    console.log(currentUser);
-    console.log(JSON.parse(localStorage.getItem("hikes")));
-    console.log(JSON.parse(localStorage.getItem("appUsers")));
-    console.log(JSON.parse(localStorage.getItem("currentUser")));
-  }, []);
-  */
+  //   console.log(hikes);
+  //   console.log(appUsers);
+  //   console.log(currentUser);
+  //   console.log(JSON.parse(localStorage.getItem("hikes")));
+  //   console.log(JSON.parse(localStorage.getItem("appUsers")));
+  //   console.log(JSON.parse(localStorage.getItem("currentUser")));
+  // }, []);
+  
 
   return (
     <GlobalContext.Provider
