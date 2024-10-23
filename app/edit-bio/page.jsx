920ef1e6bb20c15dffeb7a0c299ac99f2046bd9c
Annie McMahon <108587699+AnnieMcMahon@users.noreply.github.com>
2024-10-23 16:31:08 -0400

    removed edit email field

--- app/edit-bio/page.jsx
+++ app/edit-bio/page.jsx
@@ -10,11 +10,9 @@ function EditBio() {
   function handleSubmit(e) {
     e.preventDefault();
     const newInfo = currentUser;
-    let newEmail = e.target.email.value;
     let newName = e.target.name.value;
     let newAvatar = e.target.avatar.value;
     let newBio = e.target.bio.value;
-    if (newEmail) newInfo.email = newEmail;
     if (newName) newInfo.name = newName;
     if (newAvatar) newInfo.avatar = newAvatar;
     if (newBio) newInfo.bio = newBio;
@@ -36,9 +34,6 @@ function EditBio() {
       <h1>Edit Bio</h1>
       <div id="form-area" className="text-box">
         <form onSubmit={(e) => handleSubmit(e)}>
-          <label htmlFor="newEmail">E-mail: </label>
-          <input type="email" name="email" id="newEmail" autoComplete="true" />
-          <br />
           <label htmlFor="newName">Name: </label>
           <input type="text" name="name" id="newName"/>
           <br />
