ee901b0a572f75103640775096c2b98430ada580
Annie McMahon <108587699+AnnieMcMahon@users.noreply.github.com>
2024-10-24 12:05:14 -0400

    fixed EditBio issues

--- app/edit-bio/page.jsx
+++ app/edit-bio/page.jsx
@@ -2,10 +2,16 @@
 import "./edit-bio.css";
 import { useRouter } from "next/navigation";
 import { useGlobal } from "../context/GlobalContext";
+import { useState } from "react";
+
 
 function EditBio() {
   const router = useRouter();
   const { currentUser, setCurrentUser, appUsers, setAppUsers } = useGlobal();
+  const [bioInfo, setBioInfo] = useState({
+    name: currentUser.name,
+    bio: currentUser.bio,
+  });
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -29,19 +35,25 @@ function EditBio() {
       router.push("/edit-bio");
     }
 
+    function handleChange(e) {
+      setBioInfo((prevState) => ({
+        ...prevState, [e.target.name]: e.target.value,
+      }));
+    }
+
   return (
     <div id="edit-bio">
       <h1>Edit Bio</h1>
       <div id="form-area" className="text-box">
         <form onSubmit={(e) => handleSubmit(e)}>
           <label htmlFor="newName">Name: </label>
-          <input type="text" name="name" id="newName" value={currentUser.name || ""}/>
+          <input type="text" name="name" id="newName" value={bioInfo.name || ""} onChange={(e) => handleChange(e)}/>
           <br />
           <label htmlFor="newAvatar">Avatar: </label>
           <input type="file" accept="image/*" name="avatar" id="newAvatar" />
           <br />
           <label htmlFor="newBio">Bio: </label>
-          <textarea name="bio" id="newBio" value={currentUser.bio || ""}/>
+          <textarea name="bio" id="newBio" value={bioInfo.bio || ""} onChange={(e) => handleChange(e)}/>
           <br />
           <button type="submit" className="form-button">
             Save
